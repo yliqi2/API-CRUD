3d1e5ca1adf03fc77badc5d71019f957f6c14012
unknown <1871649909yang@alumnes.ilerna.com>
2026-02-03 19:52:00 +0100

    Inicializar el proyecto

---
+++ app/Http/Controllers/AnimalesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\animales;
+use Illuminate\Http\Request;
+
+class AnimalesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(animales $animales)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(animales $animales)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, animales $animales)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(animales $animales)
+    {
+        //
+    }
+}
