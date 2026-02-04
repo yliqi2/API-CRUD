88c11d7eae2d4f04f2a23439c42ce4e7b6f06869
Liqiang <1871649909yang@gmail.com>
2026-02-04 17:49:21 +0100

    Se ha generado las rutas mediante appResources

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalsController;
+use App\Http\Controllers\PersonController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// CRUD de Animales y Personas
+// https://antoniojenaro.com/blog/optimizacion-de-tus-rutas-api-en-laravel-el-metodo-apiresource
+// Link de donde he sacado la optimización de las rutas API
+Route::apiResources([
+    'animals' => AnimalsController::class,
+    'person' => PersonController::class,
+]);
+
+
+
